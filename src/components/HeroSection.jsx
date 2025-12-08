5e918fcb53d429ab55b380ee72d013c134f2125a
SnehansuBehera <snehansbehera080903@gmail.com>
2025-12-08 12:53:18 +0530

    few-fixes

--- src/components/HeroSection.jsx
+++ src/components/HeroSection.jsx
@@ -4,8 +4,8 @@ export default function HeroSection() {
   return (
     <div
       className="
-      relative w-full h-screen flex flex-col items-center justify-start overflow-hidden
-      bg-[#050504]
+      relative w-full h-[120vh] md:h-screen flex flex-col md:flex-row  items-center justify-between md:pr-48 lg:pr-28 2xl:pr-52
+      bg-[#050504] overflow-hidden
       "
     >
 
@@ -13,18 +13,27 @@ export default function HeroSection() {
         <div className="w-[900px] h-[900px] bg-[radial-gradient(circle,rgba(255,50,30,0.22),transparent_70%)] blur-3xl"></div>
       </div>
 
-      <div className="max-w-[900px] mt-25 xl:mt-30 2xl:mt-25 flex flex-col items-center md:mt-25 px-4 bg-transparent text-center z-10">
-        <h1 className="text-4xl md:text-7xl xl:text-5xl 2xl:text-6xl font-semibold text-white leading-tight">
-          A New Standard Designed{" "}
-          for a<span className="text-[#EE3324]"> Longer, Healthier Life.</span>
+      <div className="w-full md:max-w-[900px] lg:max-w-[800px] mt-[30vw] md:mt-0 2xl:max-w-[900px] px-6 md:px-4 md:ml-48 lg:ml-28 2xl:ml-40 lg:mt-20 flex flex-col items-start bg-transparent text-left z-10">
+        <h1 className="hidden md:block text-2xl md:text-7xl lg:text-4xl xl:text-5xl 2xl:text-[50px] font-semibold text-white lg:leading-[45px] xl:leading-[60px] 2xl:leading-[68px]">
+          A New <br />
+          Standard <br />
+          Designed for a <br />
+          <span className="text-[#EE3324]"> Longer,
+            <br /> Healthier Life.</span>
+        </h1>
+        <h1 className="w-full md:hidden text-4xl font-semibold text-white">
+          A New Standard <br />
+          Designed for a <br />
+          <span className="text-[#EE3324]"> Longer,
+            <br /> Healthier Life.</span>
         </h1>
 
-        <p className="text-[#EDEDED] px-6 md:px-0 text-base mt-4 leading-relaxed">
+        <p className="text-[#EDEDED] text-base lg:text-sm lg:w-sm xl:w-md xl:text-base mt-[5vh] md:mt-4 leading-relaxed">
           Science-backed, personalized fitness that combines training, recovery, nutrition, and performance— into one intelligent experience.
 
         </p>
 
-        <div className="flex items-center mt-8">
+        <div className="flex items-center mt-[4vh] mb-[2vh] lg:mt-6 xl:mt-8">
           <button className="px-8 py-3 rounded-full text-white text-sm font-bold bg-[#717171]">
             Explore the Platform
           </button>
@@ -41,17 +50,15 @@ export default function HeroSection() {
         </div>
       </div>
 
-      <div className="w-full flex mt-15 2xl:mt-12 justify-center pb-10 z-10">
-        <div className="w-[90%] md:w-[70%] 2xl:w-[55%] rounded-[50px] overflow-hidden">
+      <div className="mt-2 md:mt-10 z-10">
           <Image
-            src="/home-img3.png"
+            src="/sub.png"
             alt="home-img"
-            width={1400}
-            height={600}
-            className="w-full object-cover"
+            width={500}
+            height={500}
+            className="w-[60vw] lg:w-[38vw] xl:w-[40vw] 2xl:w-[33vw] object-cover"
           />
         </div>
-      </div>
 
     </div>
   );
